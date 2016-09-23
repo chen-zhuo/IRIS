d84df7f8ea7a2f6e42ec6c1412a6f374a54cc67a
l by <luo.beiyu@yahoo.com>
2016-09-23 15:45:30 +0800

    audio
    audio

--- RaspberryPi/audio.py
+++ RaspberryPi/audio.py
@@ -11,9 +11,43 @@ Plays an audio file over the earphone. The audio file is specified by 'fileNameW
 @param fileNameWithPath
            e.g. '~/Desktop/IRIS/RaspberryPi/Audio Files/Go Straight.mp3'
 '''
-def playAudioFile(fileNameWithPath):
-    # TODO
-    return
+import pygame.mixer
+from pygame.mixer import Sound
+from threading import Thread
+import random
+
+pygame.mixer.init()
+
+fileDir = '/home/pi/Documents/sound.mp3'
+
+#turnR.play()
+
+def playNum(fileDir):
+    if fileDir == '/home/pi/Documents/sound.mp3':
+        Thread(target = loadBGM).start()
+    else:
+        num = Sound(fileDir)
+        pygame.mixer.music.set_volume(0.5)
+        num.play()
+
+musicList = ['sound.mp3']
+
+def loadBGM():
+    bgm = random.choice(musicList)
+    pygame.mixer.music.set_volume(0.9)
+
+    pygame.mixer.music.load('/home/pi/Documents/sound.mp3')
+    playBGM()
+
+def playBGM():
+    pygame.mixer.music.play()
+
+#while the user is waiting
+#def playMusic():
+#Thread(target = loadBGM).start()
+Thread(target = playNum(fileDir)).start()
+print "music end"
+
 
 if __name__ == '__main__':
     pass
