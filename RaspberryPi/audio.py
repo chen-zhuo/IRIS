fd763f7978752804d7813ab951be1fc58af6c802
David CHEN ZHUO <david.chen.zhuo@gmail.com>
2016-09-23 23:25:07 +0800

    re-organize audio files

--- RaspberryPi/audio.py
+++ RaspberryPi/audio.py
@@ -2,7 +2,7 @@
 This file contains algorithms related to audio output. During navigation, the user receives audio feedback (turn-by-turn
 navigation information) over the earphone.
 
-@author: chen-zhuo
+@author: bei-yu
 '''
 
 '''
