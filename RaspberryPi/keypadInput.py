9d233e6d4074a13d3a78e501d4c6c5150cff799a
David CHEN ZHUO <david.chen.zhuo@gmail.com>
2016-10-19 03:11:38 +0800

    no message

--- RaspberryPi/keypadInput.py
+++ RaspberryPi/keypadInput.py
@@ -88,6 +88,7 @@ def _test():
         print('You have keyed in: ' + userInput)
         print('Press the hash key to confirm, or asterisk key to re-enter.')
         isUserInputConfirmed = getKeyPress()
+        print('prevKeyPressed = ' + prevKeyPressed)
         if isUserInputConfirmed == '#':
             print('Comfirmed user input: ' + userInput)
             break
