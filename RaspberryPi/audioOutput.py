51e0bb081e7d0a88c8756be18ba77c5d30dccb85
David CHEN ZHUO <david.chen.zhuo@gmail.com>
2016-10-19 22:17:07 +0800

    test audio

--- RaspberryPi/audioOutput.py
+++ RaspberryPi/audioOutput.py
@@ -193,6 +193,7 @@ def _test():
     sleep(3)
     
     print(stringHelper.MESSAGE + ' Playing multiple audio files simultaneously...')
+    sleep(0.5)
     
     print(stringHelper.AUDIO + ' ' + audioTextDict['goStraight'])
     playAudioNow('goStraight')
@@ -212,6 +213,7 @@ def _test():
     sleep(4)
     
     print(stringHelper.MESSAGE + ' Playing multiple audio files one after another...')
+    sleep(0.5)
     
     print(stringHelper.AUDIO + ' ' + audioTextDict['goStraight'])
     playAudio('goStraight')
