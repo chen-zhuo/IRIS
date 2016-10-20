0aeb711542e2ad19633202966292790c45e0eb8d
David CHEN ZHUO <david.chen.zhuo@gmail.com>
2016-10-21 02:48:55 +0800

    test

--- RaspberryPi/DummyPiMegaCommunicator.py
+++ RaspberryPi/DummyPiMegaCommunicator.py
@@ -75,6 +75,7 @@ class PiMegaCommunicator():
         self.distanceWalked_northwest = int(dataPacket.distancesList[7])
         self.heading = int(dataPacket.heading)
         
+        dataPacket = dataPacket.replace('\n', '\n    ') # add indentation
         return dataPacket
 
 def _test():
