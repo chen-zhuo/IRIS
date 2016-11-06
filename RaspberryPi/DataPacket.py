4745d51d9438828c0ac6c823a7841b3234dc7ea9
David CHEN ZHUO <david.chen.zhuo@gmail.com>
2016-11-06 14:47:33 +0800

    test

--- RaspberryPi/DataPacket.py
+++ RaspberryPi/DataPacket.py
@@ -44,9 +44,9 @@ class DataPacket:
         
         i = 0 # for traversing through `bytestream`
         
-        print(chr(bytestream[i]))
+        print(bytestream[i])
         # to parse `packetId`
-        while str(chr(bytestream[i])) != ',':
+        while bytestream[i] != ',':
             self.packetId += chr(bytestream[i])
             i = i + 1
         self.packetId = int(self.packetId)
